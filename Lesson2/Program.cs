c46b3de8f0758cb4631147605371d1d96e95a6cb
kubik6 <mursukayevr@gmail.com>
2024-08-08 08:40:57 +0400

    first

---
+++ Lesson2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("number");
+           int var1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("number");
+            int var2 = int.Parse(Console.ReadLine());
+
+            var result = Sum(var1, var2);
+            Console.WriteLine(result);
+        }
+
+        private static int Sum(int x, int y) {
+            return x + y;
+        }
+    }
+}
